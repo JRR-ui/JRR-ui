ad7a61bb41ba4ddd2ada2fbdd6997ada899f112d
JRR <441299708@qq.com>
2020-11-24 17:25:30 +0800

    homework

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
-from django.contrib import admin
-
 from . import models
 
-admin.site.register(models.Article)
-admin.site.register(models.Reporter)
+admin.site.register(models.Student)
+admin.site.register(models.Homework)
